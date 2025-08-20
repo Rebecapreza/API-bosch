3c29859ca14216307cee4229d9ab457511f612e5
Rebecapreza <prezabeca@gmail.com>
2025-08-20 11:27:44 -0300

    Aula Wilson

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response, Depends
 from models import personagemToyStory
-from typing import Optional
+from typing import Optional, Any
 
 
 
-app = FastAPI()
+app = FastAPI(title="API dos personagens de Toy Story - DS18", version='0.0.1', description="API realizada com o Wilson para aprender FastAPI")
+
+def fake_db():
+    try:
+        print("Conectadando com po banco")
+    finally:
+        print("Fechando banco")
+        
+        
 
 personagens = {
     1:{
@@ -28,10 +36,10 @@ async def raiz():
     return {"Mensagem": "Funcionou"}
 
 @app.get("/personagens")
-async def get_personagens():
+async def get_personagens(db: any = Depends(fake_db)):
     return personagens
 
-@app.get("/personagens/(personagem_id)")
+@app.get("/personagens/(personagem_id)", description="Retorna um personagem com um id específico", summary="Retorna um personagem")
 async def get_personagens(personagem_id: int):
     try:
         personagem = personagens[personagem_id]
@@ -45,6 +53,29 @@ async def post_personagem(personagem: Optional[personagemToyStory] = None):
     personagens[next_id] = personagem
     del personagem.id
     return personagem
+
+@app.put("/personagens/{personagem_id}")
+async def put_personagem(personagem_id:int, personagem: personagemToyStory):
+    if personagem_id in personagens:
+        personagens[personagem_id] = personagem
+        personagem.id = personagem.id
+        del personagem.id
+        return personagem
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
+ 
+@app.delete("/personagens/{personagem_id}")
+async def delete_personagem(personagem_id):
+    if personagem_id in personagens:
+        del personagens[personagem_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, deital="Personagem não encontrado")
+    
+@app.get ("/calcular")
+async def calcular (a: int, b: int):
+    soma = a + b
+    return "Resultado", soma
 
 if __name__ == "__main__":
     import uvicorn
